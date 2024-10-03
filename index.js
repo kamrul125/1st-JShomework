f11581a7bc3923cc1ecd7a43ab5edde58d1281a9
kamrul125 <kamrul.bubtr@gmail.com>
2024-10-03 09:57:59 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,58 @@
+// Math number 1
+ let deposited = 25;
+ let bought = 15;
+ 
+ let totalApple = deposited + bought;
+ console.log( ' Total apple : ' + totalApple );
+
+ // Math number 2
+
+ let depositedOfJone = 500;
+ let spending = 200;
+
+ let isLeft = depositedOfJone-spending;
+ console.log(' is left : ' +  isLeft);
+
+  // Math number 3
+
+  let perHourMakeMachine = 120;
+  let totalHour = 8;
+
+  let totalMacchineMake=120*8;
+  console.log(' Total Macchine: '  +  totalMacchineMake);
+
+    // Math number 4
+   let inARow = 12;
+   let totalRow = 8;
+   let moreTress = 20;
+
+   let allInAll = 12 * 8 + 20;
+
+   console.log(' All In All ' + allInAll);
+
+     // Math number 5
+     let totalBox = 5;
+     let quantity = 10;
+     let gaveToFriend = 12 ;
+
+     let thereAreOranges = 5 * 10 -12;
+
+     console.log(' there are oranges : ' + thereAreOranges);
+
+     // Math number 6
+     let beyond = 150;
+     let takesTime = 3;
+     let passesEveryHour = 150/3;
+
+     console.log('Passes Every Hour : ' + passesEveryHour)
+
+      // Math number 8
+
+      let TotalChocolate = 120;   
+      let perPackQ = 9;     
+
+      // Calculate remainder using the formula
+
+      let totalPack = Math.floor(TotalChocolate / perPackQ);
+      let remaining = TotalChocolate - (perPackQ * totalPack);
+      console.log(" Remaining: ", remaining);  
